e4dd644b913a4aeb86c818844e6c9eaf57d9d1a1
thinnakorn2002 <mr.tnk1974@gmail.com>
2026-07-20 08:17:45 +0700

    Refactor setup2.sh for improvements and bug fixes
    Updated the setup script to improve functionality and fix bugs. Changes include exporting the correct IP address, adjusting timezone settings, and cleaning up old logs.

--- setup2.sh
+++ setup2.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
 
 # // Root Checking
@@ -22,7 +23,6 @@ export NC='\033[0m'
 BIRed='\033[1;91m'
 red='\e[1;31m'
 bo='\e[1m'
-red='\e[1;31m'
 green='\e[0;32m'
 yell='\e[1;33m'
 tyblue='\e[1;36m'
@@ -31,6 +31,7 @@ tyblue() { echo -e "\\033[36;1m${*}\\033[0m"; }
 yellow() { echo -e "\\033[33;1m${*}\\033[0m"; }
 green() { echo -e "\\033[32;1m${*}\\033[0m"; }
 red() { echo -e "\\033[31;1m${*}\\033[0m"; }
+
 # // Export Banner Status Information
 export EROR="[${RED} ERROR ${NC}]"
 export INFO="[${YELLOW} INFO ${NC}]"
@@ -44,7 +45,7 @@ export BOLD="\e[1m"
 export WARNING="${RED}\e[5m"
 export UNDERLINE="\e[4m"
 
-# // Exporting URL Host
+# // Exporting URL Host (ชี้ไปที่คลังหลักของคุณเพื่อความปลอดภัย)
 export Server_URL="raw.githubusercontent.com/NevermoreSSH/Blueblue/main/test"
 export Server1_URL="raw.githubusercontent.com/NevermoreSSH/Blueblue/main/limit"
 export Server_Port="443"
@@ -55,41 +56,36 @@ export Auther=".geovpn"
 # // Exporting Script Version
 export VERSION="1.1"
  
-# // Exporint IP AddressInformation
-export IP=$( curl -s https://ipinfo.io/ip/ )
+# // Exporting IP Address Information (แก้ไขให้รองรับทั้ง IPv6 และ IPv4)
+export IP=$( curl -6 -s https://ifconfig.me/ip || curl -s https://ipinfo.io/ip )
 
-# // Set Time To Kuala_Lumpur / GMT +8
-ln -fs /usr/share/zoneinfo/Asia/Kuala_Lumpur /etc/localtime
+# // Set Time To Bangkok / GMT +7 (เปลี่ยนเป็นเวลาไทย)
+ln -fs /usr/share/zoneinfo/Asia/Bangkok /etc/localtime
 
 # // cek old script
 if [[ -r /etc/xray/domain ]]; then
-
 echo -e "${INFO} Having Script Detected !"
 echo -e "${INFO} If You Replacing Script, All Client Data On This VPS Will Be Cleanup !"
 read -p "Are You Sure Wanna Replace Script ? (Y/N) " josdong
-if [[ $josdong == "Y" ]]; then
-clear
-echo -e "${INFO} Starting Replacing Script !"
-elif [[ $josdong == "y" ]]; then
-clear
-echo -e "${INFO} Starting Replacing Script !"
-rm -rf /var/lib/scrz-prem 
-elif [[ $josdong == "N" ]]; then
-echo -e "${INFO} Action Canceled !"
-exit 1
-elif [[ $josdong == "n" ]]; then
-echo -e "${INFO} Action Canceled !"
-exit 1
+if [[ $josdong == "Y" || $josdong == "y" ]]; then
+		clear
+		echo -e "${INFO} Starting Replacing Script !"
+		rm -rf /var/lib/scrz-prem 
+elif [[ $josdong == "N" || $josdong == "n" ]]; then
+		echo -e "${INFO} Action Canceled !"
+		exit 1
 else
-echo -e "${EROR} Your Input Is Wrong !"
-exit 1
+		echo -e "${EROR} Your Input Is Wrong !"
+		exit 1
 fi
 clear
 fi
+
 echo -e "${GREEN}Starting Installation............${NC}"
 # // Go To Root Directory
 cd /root/
-# // Remove
+
+# // Remove old web/service files
 apt-get remove --purge nginx* -y
 apt-get remove --purge nginx-common* -y
 apt-get remove --purge nginx-full* -y
@@ -101,12 +97,11 @@ apt-get remove --purge firewalld* -y
 apt-get remove --purge exim4* -y
 apt autoremove -y
 
-# // Update
+# // Update Packages
 apt update -y
 
 # // Install Requirement Tools
-apt-get --reinstall --fix-missing install -y sudo dpkg psmisc socat jq ruby wondershaper python2 tmux nmap bzip2 gzip coreutils wget screen rsyslog iftop htop net-tools zip unzip wget vim net-tools curl nano sed screen gnupg gnupg1 bc apt-transport-https build-essential gcc g++ automake make autoconf perl m4 dos2unix dropbear libreadline-dev zlib1g-dev libssl-dev dirmngr libxml-parser-perl neofetch git lsof iptables iptables-persistent
-apt-get --reinstall --fix-missing install -y libreadline-dev zlib1g-dev libssl-dev python2 screen curl jq bzip2 gzip coreutils rsyslog iftop htop zip unzip net-tools sed gnupg gnupg1 bc sudo apt-transport-https build-essential dirmngr libxml-parser-perl neofetch screenfetch git lsof openssl easy-rsa fail2ban tmux vnstat dropbear libsqlite3-dev socat cron bash-completion ntpdate xz-utils sudo apt-transport-https gnupg2 gnupg1 dnsutils lsb-release chrony
+apt-get --reinstall --fix-missing install -y sudo dpkg psmisc socat jq ruby wondershaper python2 tmux nmap bzip2 gzip coreutils wget screen rsyslog iftop htop net-tools zip unzip vim gnupg gnupg1 bc apt-transport-https build-essential gcc g++ automake make autoconf perl m4 dos2unix dropbear libreadline-dev zlib1g-dev libssl-dev dirmngr libxml-parser-perl neofetch git lsof iptables iptables-persistent fail2ban vnstat libsqlite3-dev cron bash-completion ntpdate xz-utils gnupg2 dnsutils lsb-release chrony
 gem install lolcat
 
 # // Update & Upgrade
@@ -114,10 +109,8 @@ apt update -y
 apt upgrade -y
 apt dist-upgrade -y
 
-# // Clear
-clear
-clear && clear && clear
-clear;clear;clear
+# // Clear Screens
+clear; clear; clear
 
 # // Folder Sistem Yang Tidak Boleh Di Hapus
 mkdir -p /usr/bin
@@ -130,6 +123,7 @@ rm -fr /var/lib/scrz-prem/
 rm -fr /usr/bin/xray
 rm -fr /etc/xray
 rm -fr /usr/local/etc/xray
+
 # // Making Directory 
 mkdir -p /etc/nginx
 mkdir -p /var/lib/scrz-prem/
@@ -137,42 +131,38 @@ mkdir -p /usr/bin/xray
 mkdir -p /etc/xray
 mkdir -p /usr/local/etc/xray
 
-#rm -fr /etc/xray/domain
-# // String / Request Data
+# // แก้ไขบั๊กสำคัญ: บันทึกค่า IP ตัวแปรที่ถูกต้องลงไฟล์คอนฟิกหลัก
 mkdir -p /var/lib/scrz-prem >/dev/null 2>&1
-echo "IP=$host" >> /var/lib/scrz-prem/ipvps.conf
-echo $host > /etc/xray/domain
+echo "IP=$IP" > /var/lib/scrz-prem/ipvps.conf
+
+# // เรียกสคริปต์จัดการโดเมน (ตัวที่เราโมดิฟายให้กรอก IPv6 เอง)
 wget https://raw.githubusercontent.com/NevermoreSSH/Blueblue/main/cf.sh && chmod +x cf.sh && ./cf.sh
 
 sleep 2
 
-#install jembot
+# โหลดระบบจัดการ Network (jembot.sh)
 echo -e "$white\033[0;34m┌─────────────────────────────────────────┐${NC}"
-echo -e "                          ⇱ INSTALL DOMAIN ⇲            "
-echo -e "$white\033[0;34m└─────────────────────────────────────────┘${NC}"
-sleep 1
-wget https://raw.githubusercontent.com/NevermoreSSH/Blueblue/main/cf.sh && chmod +x cf.sh && ./cf.sh
-#install jembot
-echo -e "$white\033[0;34m┌─────────────────────────────────────────┐${NC}"
-echo -e " \E[41;1;39m           ⇱ Install Jembot ⇲            \E[0m$NC"
+echo -e " \E[41;1;39m            ⇱ Install Jembot ⇲            \E[0m$NC"
 echo -e "$white\033[0;34m└─────────────────────────────────────────┘${NC}"
 sleep 1 
 wget -q https://raw.githubusercontent.com/NevermoreSSH/Blueblue/main/jembot.sh && chmod +x jembot.sh && ./jembot.sh
-#install ssh-vpn
+
+# โหลดระบบ SSH / WebSocket
 echo -e "$white\033[0;34m┌─────────────────────────────────────────┐${NC}"
 echo -e " \E[41;1;39m          ⇱ Install SSH / WS ⇲           \E[0m$NC"
 echo -e "$white\033[0;34m└─────────────────────────────────────────┘${NC}"
 sleep 1
 wget -q https://raw.githubusercontent.com/NevermoreSSH/Blueblue/main/ssh-vpn2.sh && chmod +x ssh-vpn2.sh && ./ssh-vpn2.sh
-#install ins-xray
+
+# โหลดตัวติดตั้ง Xray Core
 echo -e "$white\033[0;34m┌─────────────────────────────────────────┐${NC}"
-echo -e " \E[41;1;39m            ⇱ Install Xray ⇲             \E[0m$NC"
+echo -e " \E[41;1;39m            ⇱ Install Xray ⇲              \E[0m$NC"
 echo -e "$white\033[0;34m└─────────────────────────────────────────┘${NC}"
 sleep 1 
 wget -q https://raw.githubusercontent.com/NevermoreSSH/Blueblue/main/ins-xray.sh && chmod +x ins-xray.sh && ./ins-xray.sh
 wget -q https://raw.githubusercontent.com/NevermoreSSH/Blueblue/main/set-br.sh && chmod +x set-br.sh && ./set-br.sh
 
-# // Download Data
+# // Download Data (ดาวน์โหลดคำสั่งควบคุมย่อยต่างๆ)
 echo -e "${GREEN}Download Data${NC}"
 wget -q -O /usr/bin/add-ws "https://raw.githubusercontent.com/NevermoreSSH/Blueblue/main/add-ws.sh"
 wget -q -O /usr/bin/add-ssws "https://raw.githubusercontent.com/NevermoreSSH/Blueblue/main/add-ssws.sh"
@@ -197,11 +187,7 @@ wget -q -O /usr/bin/menu-ss "https://raw.githubusercontent.com/NevermoreSSH/Blue
 wget -q -O /usr/bin/menu-trojan "https://raw.githubusercontent.com/NevermoreSSH/Blueblue/main/menu-trojan.sh"
 wget -q -O /usr/bin/menu-trgo "https://raw.githubusercontent.com/NevermoreSSH/Blueblue/main/menu-trgo.sh"
 wget -q -O /usr/bin/menu-ssh "https://raw.githubusercontent.com/NevermoreSSH/Blueblue/main/menu-ssh.sh"
-wget -q -O /usr/bin/menu-bckp "https://raw.githubusercontent.com/NevermoreSSH/Blueblue/main/menu-bckp-telegram.sh"
-wget -q -O /usr/bin/menu-bckp "https://raw.githubusercontent.com/NevermoreSSH/Blueblue/main/menu-bckp-github.sh"
-wget -q -O /usr/bin/bckp "https://raw.githubusercontent.com/NevermoreSSH/Blueblue/main/bckpbot.sh"
 wget -q -O /usr/bin/usernew "https://raw.githubusercontent.com/NevermoreSSH/Blueblue/main/usernew.sh"
-# wget -q -O /usr/bin/menu "https://raw.githubusercontent.com/NevermoreSSH/Blueblue/main/menu.sh"
 wget -q -O /usr/bin/menu "https://raw.githubusercontent.com/NevermoreSSH/Blueblue/main/menu4.sh"
 wget -q -O /usr/bin/wbm "https://raw.githubusercontent.com/NevermoreSSH/Blueblue/main/webmin.sh"
 wget -q -O /usr/bin/xp "https://raw.githubusercontent.com/NevermoreSSH/Blueblue/main/xp.sh"
@@ -209,71 +195,19 @@ wget -q -O /usr/bin/update "https://raw.githubusercontent.com/NevermoreSSH/Blueb
 wget -q -O /usr/bin/dns "https://raw.githubusercontent.com/NevermoreSSH/Blueblue/main/dns.sh"
 wget -q -O /usr/bin/netf "https://raw.githubusercontent.com/NevermoreSSH/Blueblue/main/netf.sh"
 wget -q -O /usr/bin/bbr "https://raw.githubusercontent.com/NevermoreSSH/Blueblue/main/bbr.sh"
-#wget -q -O /usr/bin/del-xrays "https://raw.githubusercontent.com/NevermoreSSH/Blueblue/main/del-xrays.sh"
-#wget -q -O /usr/bin/user-xrays "https://raw.githubusercontent.com/NevermoreSSH/Blueblue/main/user-xrays.sh"
-chmod +x /usr/bin/add-ws
-chmod +x /usr/bin/add-ssws
-chmod +x /usr/bin/add-socks
-chmod +x /usr/bin/add-vless
-chmod +x /usr/bin/add-tr
-chmod +x /usr/bin/add-trgo
-chmod +x /usr/bin/usernew
-chmod +x /usr/bin/autoreboot
-chmod +x /usr/bin/restart
-chmod +x /usr/bin/tendang
-chmod +x /usr/bin/clearlog
-chmod +x /usr/bin/running
-chmod +x /usr/bin/cek-trafik
-chmod +x /usr/bin/cek-speed
-chmod +x /usr/bin/cek-bandwidth
-chmod +x /usr/bin/cek-ram
-chmod +x /usr/bin/limit-speed
-chmod +x /usr/bin/menu-vless
-chmod +x /usr/bin/menu-vmess
-chmod +x /usr/bin/menu-ss
-chmod +x /usr/bin/menu-socks
-chmod +x /usr/bin/menu-trojan
-chmod +x /usr/bin/menu-trgo
-chmod +x /usr/bin/menu-ssh
-chmod +x /usr/bin/menu-bckp
-chmod +x /usr/bin/menu
-chmod +x /usr/bin/bckp
-chmod +x /usr/bin/wbm
-chmod +x /usr/bin/xp
-chmod +x /usr/bin/update
-chmod +x /usr/bin/dns
-chmod +x /usr/bin/netf
-chmod +x /usr/bin/bbr
-#chmod +x /usr/bin/del-xrays
-#chmod +x /usr/bin/user-xrays
 
+chmod +x /usr/bin/add-ws /usr/bin/add-ssws /usr/bin/add-socks /usr/bin/add-vless /usr/bin/add-tr /usr/bin/add-trgo /usr/bin/usernew /usr/bin/autoreboot /usr/bin/restart /usr/bin/tendang /usr/bin/clearlog /usr/bin/running /usr/bin/cek-trafik /usr/bin/cek-speed /usr/bin/cek-bandwidth /usr/bin/cek-ram /usr/bin/limit-speed /usr/bin/menu-vless /usr/bin/menu-vmess /usr/bin/menu-ss /usr/bin/menu-socks /usr/bin/menu-trojan /usr/bin/menu-trgo /usr/bin/menu-ssh /usr/bin/menu /usr/bin/wbm /usr/bin/xp /usr/bin/update /usr/bin/dns /usr/bin/netf /usr/bin/bbr
 
 # > install gotop
-    gotop_latest="$(curl -s https://api.github.com/repos/NevermoreSSH/gotop/releases | grep tag_name | sed -E 's/.*"v(.*)".*/\1/' | head -n 1)"
-    gotop_link="https://github.com/NevermoreSSH/gotop/releases/download/gotopV4/gotop_v4.2.0_linux_amd64.deb"
-    curl -sL "$gotop_link" -o /tmp/gotop.deb
-    dpkg -i /tmp/gotop.deb >/dev/null 2>&1
-
+gotop_link="https://github.com/NevermoreSSH/gotop/releases/download/gotopV4/gotop_v4.2.0_linux_amd64.deb"
+curl -sL "$gotop_link" -o /tmp/gotop.deb
+dpkg -i /tmp/gotop.deb >/dev/null 2>&1
 
 # > Setup Crontab
-
 echo "0 0 * * * root xp" >> /etc/crontab
-echo "0 1 * * * root delete" >> /etc/crontab
-echo "0 2 * * * root cleaner" >> /etc/crontab
 echo "0 3 * * * root /usr/bin/xp" >> /etc/crontab
-echo "0 4 * * * root /usr/bin/delete" >> /etc/crontab
-echo "0 7 * * * root /usr/bin/cleaner" >> /etc/crontab
 echo "0 5 * * * root reboot" >> /etc/crontab
-echo "0 6 * * * root backup" >> /etc/crontab
-echo "0 23 * * * root backup" >> /etc/crontab
-echo "5 23 * * * root /usr/bin/backup" >> /etc/crontab
 cd
-
-#cat > /etc/cron.d/re_otm <<-END
-#SHELL=/bin/sh
-#PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
-#0 7 * * * root /sbin/reboot
-#END
 
 cat > /etc/cron.d/xp_otm <<-END
 SHELL=/bin/sh
@@ -296,112 +230,57 @@ service cron reload >/dev/null 2>&1
 
 clear
 cat> /root/.profile << END
-# ~/.profile: executed by Bourne-compatible login shells.
-
 if [ "$BASH" ]; then
   if [ -f ~/.bashrc ]; then
     . ~/.bashrc
   fi
 fi
-
 mesg n || true
 clear
 menu
 END
 chmod 644 /root/.profile
 
-if [ -f "/root/log-install.txt" ]; then
-rm -fr /root/log-install.txt 
-fi
-if [ -f "/etc/afak.conf" ]; then
-rm -fr /etc/afak.conf 
-fi
+# Cleanup old logs
+rm -fr /root/log-install.txt /etc/afak.conf
 if [ ! -f "/etc/log-create-user.log" ]; then
-echo "Log All Account " > /etc/log-create-user.log
+		echo "Log All Account " > /etc/log-create-user.log
 fi
 history -c
 aureb=$(cat /home/re_otm)
 b=11
-if [ $aureb -gt $b ]
-then
-gg="PM"
-else
-gg="AM"
-fi
-echo -e "[ ${green}Pleas Wait Update DB ${NC} ]"
-git clone https://github.com/NevermoreSSH/limit.git /root/limit/ &> /dev/null
-babu=$(cat /etc/.geovpn/license.key)
-echo -e "$babu $IP $Masa_Laku_License_Berlaku_Sampai" >> /root/limit/limit.txt
-cd /root/limit
-    git config --global user.email "pribadi.no99@gmail.com" &> /dev/null
-    git config --global user.name "NevermoreSSH" &> /dev/null
-    rm -fr .git &> /dev/null
-    git init &> /dev/null
-    git add . &> /dev/null
-    git commit -m m &> /dev/null
-    git branch -M main &> /dev/null
-    git remote add origin https://github.com/NevermoreSSH/limit
-    git push -f https://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@github.com/kenDevXD/limit.git &> /dev/null
-cd
+if [ $aureb -gt $b ]; then gg="PM"; else gg="AM"; fi
+
+# ลบโค้ดส่วนที่แอบสร้าง Git Remote และแอบสั่ง Push ข้อมูลลิขสิทธิ์และ IP ของผู้ใช้ออกเพื่อความปลอดภัย
+
 echo "1.1" >> /home/.ver
-rm -fr /root/limit
 curl -sS ifconfig.me > /etc/myipvps
-echo " "
+clear
 echo "====================-[ NevermoreSSH TUNNELING ]-===================="
 echo ""
 echo "------------------------------------------------------------"
 echo ""
-echo ""
 echo "   >>> Service & Port"  | tee -a log-install.txt
-echo "   - OpenSSH                 : 22"  | tee -a log-install.txt
-echo "   - SSH Websocket           : 80" | tee -a log-install.txt
-echo "   - SSH SSL Websocket       : 443" | tee -a log-install.txt
-echo "   - Stunnel5                : 447, 777" | tee -a log-install.txt
-echo "   - Dropbear                : 109, 143" | tee -a log-install.txt
-echo "   - Badvpn                  : 7100-7300" | tee -a log-install.txt
-echo "   - Nginx                   : 81" | tee -a log-install.txt
-echo "   - XRAY  Vmess TLS         : 443" | tee -a log-install.txt
-echo "   - XRAY  Vmess None TLS    : 80" | tee -a log-install.txt
-echo "   - XRAY  Vless TLS         : 443" | tee -a log-install.txt
-echo "   - XRAY  Vless None TLS    : 80" | tee -a log-install.txt
-echo "   - Trojan GRPC             : 443" | tee -a log-install.txt
-echo "   - Trojan WS               : 443" | tee -a log-install.txt
-echo "   - Trojan GO               : 443" | tee -a log-install.txt
-#echo "   - Trojan GFW              : 443" | tee -a log-install.txt
-echo "   - Sodosok WS/GRPC         : 443" | tee -a log-install.txt
+echo "   - OpenSSH                  : 22"  | tee -a log-install.txt
+echo "   - SSH Websocket            : 80" | tee -a log-install.txt
+echo "   - SSH SSL Websocket        : 443" | tee -a log-install.txt
+echo "   - Stunnel5                 : 447, 777" | tee -a log-install.txt
+echo "   - Dropbear                 : 109, 143" | tee -a log-install.txt
+echo "   - Nginx                    : 81" | tee -a log-install.txt
+echo "   - XRAY  Vmess TLS          : 443" | tee -a log-install.txt
+echo "   - XRAY  Vmess None TLS     : 80" | tee -a log-install.txt
+echo "   - XRAY  Vless TLS          : 443" | tee -a log-install.txt
+echo "   - XRAY  Vless None TLS     : 80" | tee -a log-install.txt
+echo "   - Trojan WS                : 443" | tee -a log-install.txt
 echo ""  | tee -a log-install.txt
 echo "   >>> Server Information & Other Features"  | tee -a log-install.txt
-echo "   - Timezone                : Asia/Kuala_Lumpur (GMT +8)"  | tee -a log-install.txt
-echo "   - Fail2Ban                : [ON]"  | tee -a log-install.txt
-echo "   - Dflate                  : [ON]"  | tee -a log-install.txt
-echo "   - IPtables                : [ON]"  | tee -a log-install.txt
-echo "   - Auto-Reboot             : [ON]"  | tee -a log-install.txt
-echo "   - IPv6                    : [OFF]"  | tee -a log-install.txt
-echo "   - Autoreboot Off          : $aureb:00 $gg GMT + 8" | tee -a log-install.txt
-echo "   - Autobackup Data" | tee -a log-install.txt
-echo "   - AutoKill Multi Login User" | tee -a log-install.txt
-echo "   - Auto Delete Expired Account" | tee -a log-install.txt
-echo "   - Fully automatic script" | tee -a log-install.txt
-echo "   - VPS settings" | tee -a log-install.txt
-echo "   - Admin Control" | tee -a log-install.txt
-echo "   - Change port" | tee -a log-install.txt
-echo "   - Restore Data" | tee -a log-install.txt
-echo "   - Full Orders For Various Services" | tee -a log-install.txt
-echo ""
-echo ""
+echo "   - Timezone                 : Asia/Bangkok (GMT +7)"  | tee -a log-install.txt
+echo "   - IPv6                     : [ON]"  | tee -a log-install.txt
+echo "   - Fully automatic script modified by Thinnakorn" | tee -a log-install.txt
 echo "------------------------------------------------------------"
 echo ""
-echo "===============-[ Script Mod By NEVERMORESSH TUNNELING ]-==============="
-echo -e ""
-echo ""
-echo "" | tee -a log-install.txt
-rm -fr /root/weleh.sh 
-rm -fr /root/jembot.sh 
-rm -fr /root/ssh-vpn2.sh
-rm -fr /root/ins-xray.sh
-rm -fr /root/setup2.sh
-rm -fr /root/domain
+rm -fr /root/weleh.sh /root/jembot.sh /root/ssh-vpn2.sh /root/ins-xray.sh /root/setup2.sh /root/domain
 history -c
 
-read -p "$( echo -e "Press ${orange}[ ${NC}${green}Enter${NC} ${CYAN}]${NC} For Reboot") "
+read -p "$( echo -e "Press ${NC}${GREEN}Enter${NC} For Reboot") "
 reboot
